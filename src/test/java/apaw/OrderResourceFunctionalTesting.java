de7964a6c4fd1f864dabacc93de2c606762d60fa
dianagtf <diana.gtf88@gmail.com>
2018-01-13 20:22:29 +0100

    Create Order test

---
+++ src/test/java/apaw/OrderResourceFunctionalTesting.java
@@ -0,0 +1,31 @@
+package apaw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import apaw.api.daos.DaoFactory;
+import apaw.api.daos.memory.DaoMemoryFactory;
+import apaw.api.resources.OrderResource;
+import apaw.http.HttpClientService;
+import apaw.http.HttpMethod;
+import apaw.http.HttpRequest;
+import apaw.http.HttpRequestBuilder;
+
+public class OrderResourceFunctionalTesting {
+	
+	@Before
+	public void before() {
+		DaoFactory.setFactory(new DaoMemoryFactory());
+	}
+	
+	private void createOrder() {
+		HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(OrderResource.ORDERS).body("1").build();
+		new HttpClientService().httpRequest(request);
+	}
+	
+	@Test
+	public void testCreateProfessional() {
+		this.createOrder();
+	}
+
+}
