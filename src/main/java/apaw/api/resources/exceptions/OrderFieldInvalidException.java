c4aeb1d62cab75fa5a576bbe75964ffb6673af3f
dianagtf <diana.gtf88@gmail.com>
2018-01-14 09:51:18 +0100

    Fix Warnings: adding serialVersionUID

--- src/main/java/apaw/api/resources/exceptions/OrderFieldInvalidException.java
+++ src/main/java/apaw/api/resources/exceptions/OrderFieldInvalidException.java
@@ -2,7 +2,9 @@ package apaw.api.resources.exceptions;
 
 public class OrderFieldInvalidException extends Exception {
 	
-public static final String DESCRIPTION = "**Id of order not found";
+	private static final long serialVersionUID = 4613089317941782288L;
+	
+	public static final String DESCRIPTION = "**Id of order not found";
 	
 	public OrderFieldInvalidException(String detail) {
 		super(DESCRIPTION + ". " + detail);
--- src/main/java/apaw/api/resources/exceptions/ProfessionalFieldInvalidException.java
+++ src/main/java/apaw/api/resources/exceptions/ProfessionalFieldInvalidException.java
@@ -2,6 +2,8 @@ package apaw.api.resources.exceptions;
 
 public class ProfessionalFieldInvalidException extends Exception{
 	
+	private static final long serialVersionUID = 985873305504040195L;
+	
 	public static final String DESCRIPTION = "**Professional phone not found";
 	
 	public ProfessionalFieldInvalidException(String detail) {
